75cd681696266554f92ae495c6d2d0c9725cbbb4
stevengweha <ngwehasteve@gmail.com>
2025-07-15 02:55:21 +0200

    update dist&app

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const path = require('path');
 
 module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-  // Vous pouvez personnaliser la config ici si besoin
+
+  // Ajout d'un loader pour les fichiers de polices (.ttf, .woff, etc)
+  config.module.rules.push({
+    test: /\.(ttf|woff|woff2|eot)$/,
+    use: [
+      {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts/',    // dossier où les fonts seront copiées
+          publicPath: '/fonts/',   // chemin public utilisé dans la build
+        },
+      },
+    ],
+  });
+
   return config;
 };
