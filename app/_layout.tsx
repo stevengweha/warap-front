0f3d085a04034d74dab43016b624cbcebac878d9
stevengweha <ngwehasteve@gmail.com>
2025-07-15 02:44:55 +0200

    update dist&app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,18 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Analytics } from '@vercel/analytics/react';
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 
 
 export default function RootLayout() {
+   const [fontsLoaded] = useFonts({
+    ...Ionicons.font,
+  });
+
+  if (!fontsLoaded) {
+    return null; // Ou un loader personnalisé pendant le chargement des polices
+  }
   return (
     <>
       <Stack />
