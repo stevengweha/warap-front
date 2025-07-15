9e248c123756c6f9424f442e578fba7cf334e30c
stevengweha <ngwehasteve@gmail.com>
2025-07-15 02:12:32 +0200

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,7 @@
+import { Analytics } from '@vercel/analytics/react';
 import { Stack } from "expo-router";
+
+
 
 export default function RootLayout() {
   return (
@@ -7,7 +10,7 @@ export default function RootLayout() {
       {/* Note: The InfoBulleIA component is not rendered here, it should be included in the layout if needed. */}
       {/* If you want to include it, you can add it inside the return statement of the RootLayout function. */}
       {/* <InfoBulleIA /> */}
-      
+      <Analytics />
     </>
   );
 }
